201c90537b0000688072d8aa45a548d0b2f00d2b
Luis Molina <luisemr14molina@gmail.com>
2023-07-10 18:14:04 -0400

    'Nav y footer'

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,25 @@
 import './styles/globals.css'
 import { Nav } from "./components/Nav/Nav"
+import { Footer } from './components/Footer/Footer'
+
 export const metadata = {
   title: 'Devsafío',
   description: 'title',
 }
 
 export default function RootLayout({ children }) {
+
  return (
     <html lang="en">
+        
       <body>
         <Nav />
-        {children}
-        </body>
+          <section>        
+            {children}
+          </section>
+        <Footer />
+      </body>
+        
     </html>
   )
 }
