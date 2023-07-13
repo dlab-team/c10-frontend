1b6e8306341f33588a30231a849b2bfccf601a85
Ezzeorazi <ezequiel.orazi90@gmail.com>
2023-07-13 17:24:10 -0300

    mm

--- src/app/views/Signin/page.jsx
+++ src/app/views/Signin/page.jsx
@@ -1,5 +1,36 @@
 export default function Signin (){
     return(
+        <div>
+            <br />
         <h1>Esto es el Signin</h1>
+        <form >
+          <label>
+            Nombre:
+            <input type="text" name="nombre"  />
+          </label>
+          <br />
+          <label>
+            Apellido:
+            <input type="text" name="apellido"/>
+          </label>
+          <br />
+          <label>
+            Email:
+            <input type="email" name="email"  />
+          </label>
+          <br />
+          <label>
+            Contraseña:
+            <input type="password" name="password"  />
+          </label>
+          <br />
+          <label>
+            Confirmar Contraseña:
+            <input type="password" name="confirmPassword" />
+          </label>
+          <br />
+          <button type="submit">Registrarse</button>
+        </form>
+      </div>
     )
 }
