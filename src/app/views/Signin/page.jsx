7cbce8b4f99807e03c8346c4b3166ec8e86efd18
Ezzeorazi <ezequiel.orazi90@gmail.com>
2023-07-13 17:40:25 -0300

    Actualizacion de formulario de registro(solo estructura

--- src/app/views/Signin/page.jsx
+++ src/app/views/Signin/page.jsx
@@ -1,36 +1,36 @@
-export default function Signin (){
-    return(
-        <div>
-            <br />
-        <h1>Esto es el Signin</h1>
-        <form >
-          <label>
-            Nombre:
-            <input type="text" name="nombre"  />
-          </label>
-          <br />
-          <label>
-            Apellido:
-            <input type="text" name="apellido"/>
-          </label>
-          <br />
-          <label>
-            Email:
-            <input type="email" name="email"  />
-          </label>
-          <br />
-          <label>
-            Contraseña:
-            <input type="password" name="password"  />
-          </label>
-          <br />
-          <label>
-            Confirmar Contraseña:
-            <input type="password" name="confirmPassword" />
-          </label>
-          <br />
-          <button type="submit">Registrarse</button>
-        </form>
-      </div>
-    )
+export default function Signin() {
+  return (
+    <div>
+      <br />
+      <h1>Registrate</h1>
+      <form>
+        <label>
+          Nombre:
+          <input type="text" placeholder=" Nombre" />
+        </label>
+        <br />
+        <label>
+          Apellido:
+          <input type="text" placeholder=" Apellido" />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input type="email" placeholder=" Email" />
+        </label>
+        <br />
+        <label>
+          Contraseña:
+          <input type="password" placeholder=" Password" />
+        </label>
+        <br />
+        <label>
+          Confirmar Contraseña:
+          <input type="password" placeholder=" Confirm Password" />
+        </label>
+        <br />
+        <button type="submit">Registrarse</button>
+      </form>
+    </div>
+  );
 }
