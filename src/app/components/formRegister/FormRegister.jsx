47ccc044fbad2f6443570ddac6da694b16edfad5
Patricio Dunstan <patriciodunstan@MacBook-Pro-de-Patricio.local>
2023-08-10 16:50:11 -0400

    actualización de formulario registro

--- src/app/components/formRegister/FormRegister.jsx
+++ src/app/components/formRegister/FormRegister.jsx
@@ -2,35 +2,42 @@ export default function FormRegister() {
   return (
     <>
       <form className="border-0 h-616 w-1104 mt-4 justify-center">
-        <div className=" flex flex-col items-center mb-4 top-0">
-          <h1 className="text-slate-900 text-5xl font-semibold">Registrate</h1>
-        </div>
-        <div>
-          <div className="grid grid-cols-4 content-center">
-            <p className="text-neutral-800 text-2xl font-normal">Nombre:</p>
-            <input type="text" placeholder=" Nombre" className="rec" />
+        <div className="bg-white border-10 border-purple-900 rounded-2xl h-96 w-4/5 max-w-lg">
+          <div className=" flex flex-col items-center mb-4 top-0">
+            <h1 className="text-slate-900 text-5xl font-semibold">
+              Registrate
+            </h1>
+          </div>
+          <div className="flex flex-row content-center">
+            <div className="grid content-center">
+              <p className="text-neutral-800 text-2xl font-normal">Nombre:</p>
+              <input
+                type="text"
+                className="relative h-12 w-64 top-0 left-0 bg-blue-200 border-purple-900  border-opacity-50 rounded-lg"
+              />
+            </div>
+            <div>
+              <p>Apellido:</p>
+              <input type="text" placeholder=" Apellido" />
+            </div>
           </div>
           <div>
-            <p>Apellido:</p>
-            <input type="text" placeholder=" Apellido" />
-          </div>
-        </div>
-        <div>
-          <label>
-            <p>Email:</p>
-            <input type="email" placeholder=" Email" />
-          </label>
-          <label>
-            <p>Contraseña:</p>
-            <input type="password" placeholder=" Password" />
-          </label>
+            <label>
+              <p>Email:</p>
+              <input type="email" placeholder=" Email" />
+            </label>
+            <label>
+              <p>Contraseña:</p>
+              <input type="password" placeholder=" Password" />
+            </label>
 
-          <label>
-            <p>Confirmar Contraseña:</p>
-            <input type="password" placeholder=" Confirm Password" />
-          </label>
+            <label>
+              <p>Confirmar Contraseña:</p>
+              <input type="password" placeholder=" Confirm Password" />
+            </label>
+          </div>
+          <button type="submit">Registrarse</button>
         </div>
-        <button type="submit">Registrarse</button>
       </form>
     </>
   )
--- src/app/views/Signin/page.jsx
+++ src/app/views/Signin/page.jsx
@@ -1,18 +1,18 @@
 import FormRegister from "@devsafio/app/components/formRegister/FormRegister"
-import Image from "next/image"
+// import background from "../../util/image/backgroundimage.png"
 
 export default function Signin() {
   return (
-    <div className="h-screen">
-      <div className="absolute -z-10">
-        <Image
-          src="/src/public/backgroundimage.png"
-          width={1440}
-          height={880}
-          quality={100}
-        />
+    <div className="relative flex flex-col items-center justify-between w-screen h-screen">
+      <div
+        className="absolute top-0 left-0 w-full h-full bg-cover z-0"
+        style={{
+          backgroundImage: `url('../../../public/backgroundimage.png')`
+        }}
+      ></div>
+      <div className="z-10 flex flex-col items-center mt-4 bg-white border-10 border-purple-900 rounded-2xl h-96 w-4/5 max-w-lg">
+        <FormRegister className="mx-auto" />
       </div>
-      <FormRegister />
     </div>
   )
 }
