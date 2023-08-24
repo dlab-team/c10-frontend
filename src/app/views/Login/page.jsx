cea2ee00c5f1e3b075c3dc0e61b7ce3a4bfdb124
Javier Arroyo <arroyojavier.sanandres@gmail.com>
2023-08-24 19:50:32 -0400

    c10-17 estructura pagina login

--- src/app/views/Login/page.jsx
+++ src/app/views/Login/page.jsx
@@ -1,9 +1,61 @@
-const Login = () => {
+'use client'
+import { useState } from 'react';
+import styles from './styles.module.css';
+
+export default function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState('');
+
+  const validateEmail = (email) => {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (!emailRegex.test(email)) {
+      setEmailError('Invalid email format');
+    } else {
+      setEmailError('');
+    }
+  };
+
+  const handleEmailChange = (e) => {
+    const newEmail = e.target.value;
+    setEmail(newEmail);
+    validateEmail(newEmail);
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Realiza la autenticación si la validación de formato pasó
+    if (!emailError) {
+      // Realizar la lógica de autenticación
+    }
+  };
+
   return (
     <>
-      <h1>Esto es el Login</h1>
+      <div className={styles.container}>
+        <form className={styles.form} onSubmit={handleLogin}>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              className={styles.inputs}
+              type="text"
+              id="email"
+              value={email}
+              onChange={handleEmailChange}
+            />
+            {emailError && <p style={{ color: 'red' }}>{emailError}</p>}
+          </div>
+          <div>
+            <label htmlFor="password">Contraeña</label>
+            <input
+              type='password'
+              id="password"
+            />
+          </div>
+          {/* Otros campos del formulario */}
+          <button type="submit">Login</button>
+        </form>
+      </div>
     </>
   )
 }
-
-export default Login
