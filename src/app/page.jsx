45fcff1844413654404ea26bf71e216725f15392
LuisMolinar <luisemr14molina@gmail.com>
2023-08-26 19:30:54 -0400

    '24-LMolina'

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,8 @@
 import Image from "next/image"
 import Imagen from "../public/Imagen.png"
+import Dev1 from "../public/Dev1.png"
+import Dev2 from "../public/Dev2.png"
+import Dev3 from "../public/Dev3.png"
 import Intro from "./components/Intro/Intro"
 
 export default function Page() {
@@ -37,8 +40,8 @@ export default function Page() {
       </div>
 
       <div className="container mx-auto flex items-center justify-center top-[880px] w-full h-479">
-        <div className="border-2 flex items-center justify-items-stretch w-1200 h-340">
-          <div className="border-2 w-1/2 p-2">
+        <div className="flex items-center justify-items-stretch w-1200 h-340">
+          <div className=" w-1/2 p-2">
             <h1 className="text-center text-3xl font-bold">
               ¿Cómo funcionamos?
             </h1>
@@ -49,10 +52,62 @@ export default function Page() {
               células tecnológicas.
             </p>
           </div>
-          <div className="border-2 w-1/2 p-2">
+          <div className=" w-1/2 p-2">
             <Intro videoId="watch?v=S_oLr_np4S8&t=2719s" />
           </div>
         </div>
+      </div>
+
+      <div className=" bg-claro lk top-[1359px] w-full h-956">
+        <br></br>
+        <br></br>
+        <div className="ml-32  w-1200 h-204">
+            <h1 className="text-center text-3xl font-bold">
+                Sin Juniors no hay Seniors
+            </h1>
+            <br></br>
+            <p className="text-2xl text-center">En este programa unimos la rapidez, eficiencia y visión de un desarrollador junior junto con la experiencia y liderazgo de un senior TI, quien acompañará al talento junior contratado para potenciar tu área digital, acá te explicamos cómo:</p>
+         </div>
+  
+        <div className="flex items-center justify-center ">
+        <div className="w-285 h-286">
+            <h1 className="text-center text-3xl font-bold">
+            Aceleramos
+            </h1>
+            <br></br>
+            <br></br>
+            <p className="text-center text-2xl">
+            Aceleramos el proceso de contratación. 
+            Una vez que nos envíes tu necesidad de talento digital recibirás 
+            un listado con los mejores candidatos en 5 días.
+            </p>
+        </div>
+        <div className="w-285 h-286">
+            <h1 className="text-center text-3xl font-bold">
+            Acompañamos
+            </h1>
+            <br></br>
+            <br></br>
+            <p className="text-center text-2xl">
+            Nosotros nos hacemos cargo, tú creces. 
+            Todos los desarrolladores que contrates serán guiados 
+            y apoyados a nivel técnico por un mentor senior TI experto en tu área sin ningún costo ($0).
+            </p>
+        </div>
+        <div className="w-285 h-286">
+            <h1 className="text-center text-3xl font-bold">
+            Potenciamos
+            </h1>
+            <br></br>
+            <br></br>
+            <p className="text-center text-2xl">
+            Potenciamos y retenemos el talento.
+            Realizamos un seguimiento de alta calidad para potenciar
+            el crecimiento y desarrollo técnico de todos los desarrolladores contratados.
+            </p>
+        </div>
+        </div>
+        
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
   theme: {
     colors: {
       azul: "#2738F5",
+      claro: "#E2F2FE",
     },
     extend: {
       backgroundImage: {
@@ -18,13 +19,18 @@ module.exports = {
       height: {
         90: "90px",
         108: "108px",
+        204: "204px",
+        216: "216px",
         784: "784px",
         479: "479px",
         340: "340px",
         170: "170px",
+        956: "956px"
       },
       width: {
+        285:"285px",
         582: "582px",
+        556: "556px",
         708: "708px",
         712: "712px",
         1200: "1200px",
